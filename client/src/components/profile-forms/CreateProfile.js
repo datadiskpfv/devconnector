9e61260aa0a645d65bc996b7d884866656488094
Paul Valle <paul.valle@datadisk.co.uk>
2019-11-02 19:19:42 +0000

    install prop-types module

--- client/src/components/profile-forms/CreateProfile.js
+++ client/src/components/profile-forms/CreateProfile.js
@@ -1,10 +1,10 @@
 import React, {Fragment, useState} from 'react';
-import { Link, withRouter } from 'react-router-dom';
+import {Link, withRouter} from 'react-router-dom';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { createProfile} from "../../actions/profile";
+import {connect} from 'react-redux';
+import {createProfile} from "../../actions/profile";
 
-const CreateProfile = ({ createProfile, history }) => {
+const CreateProfile = ({createProfile, history}) => {
 
     const [formData, setFormData] = useState({
         company: "",
@@ -38,7 +38,7 @@ const CreateProfile = ({ createProfile, history }) => {
         instagram,
     } = formData;
 
-    const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
+    const onChange = e => setFormData({...formData, [e.target.name]: e.target.value});
 
     const onSubmit = e => {
         e.preventDefault();
@@ -51,7 +51,7 @@ const CreateProfile = ({ createProfile, history }) => {
                 Create Your Profile
             </h1>
             <p className="lead">
-                <i className="fas fa-user"></i> Let's get some information to make your
+                <i className="fas fa-user"/> Let's get some information to make your
                 profile stand out
             </p>
             <small>* = required field</small>
@@ -73,19 +73,18 @@ const CreateProfile = ({ createProfile, history }) => {
                     >
                 </div>
                 <div className="form-group">
-                    <input type="text" placeholder="Company" name="company" value={company} onChange={e => onChange(e)}/>
-                    <small className="form-text"
-                    >Could be your own company or one you work for</small
-                    >
+                    <input type="text" placeholder="Company" name="company" value={company}
+                           onChange={e => onChange(e)}/>
+                    <small className="form-text">Could be your own company or one you work for</small>
                 </div>
                 <div className="form-group">
-                    <input type="text" placeholder="Website" name="website" value={website} onChange={e => onChange(e)}/>
-                    <small className="form-text"
-                    >Could be your own or a company website</small
-                    >
+                    <input type="text" placeholder="Website" name="website" value={website}
+                           onChange={e => onChange(e)}/>
+                    <small className="form-text">Could be your own or a company website</small>
                 </div>
                 <div className="form-group">
-                    <input type="text" placeholder="Location" name="location" value={location} onChange={e => onChange(e)}/>
+                    <input type="text" placeholder="Location" name="location" value={location}
+                           onChange={e => onChange(e)}/>
                     <small className="form-text"
                     >City & state suggested (eg. Boston, MA)</small
                     >
@@ -110,12 +109,13 @@ const CreateProfile = ({ createProfile, history }) => {
                     >
                 </div>
                 <div className="form-group">
-                    <textarea placeholder="A short bio of yourself" name="bio" value={bio} onChange={e => onChange(e)}></textarea>
+                    <textarea placeholder="A short bio of yourself" name="bio" value={bio} onChange={e => onChange(e)}/>
                     <small className="form-text">Tell us a little about yourself</small>
                 </div>
 
                 <div className="my-2">
-                    <button onClick={() => toggleSocialInputs(!displaySocialInputs)} type="button" className="btn btn-light">
+                    <button onClick={() => toggleSocialInputs(!displaySocialInputs)} type="button"
+                            className="btn btn-light">
                         Add Social Network Links
                     </button>
                     <span>Optional</span>
@@ -124,28 +124,33 @@ const CreateProfile = ({ createProfile, history }) => {
                 {displaySocialInputs && (
                     <Fragment>
                         <div className="form-group social-input">
-                            <i className="fab fa-twitter fa-2x"></i>
-                            <input type="text" placeholder="Twitter URL" name="twitter" value={twitter} onChange={e => onChange(e)}/>
+                            <i className="fab fa-twitter fa-2x"/>
+                            <input type="text" placeholder="Twitter URL" name="twitter" value={twitter}
+                                   onChange={e => onChange(e)}/>
                         </div>
 
                         <div className="form-group social-input">
-                            <i className="fab fa-facebook fa-2x"></i>
-                            <input type="text" placeholder="Facebook URL" name="facebook" value={facebook} onChange={e => onChange(e)}/>
+                            <i className="fab fa-facebook fa-2x"/>
+                            <input type="text" placeholder="Facebook URL" name="facebook" value={facebook}
+                                   onChange={e => onChange(e)}/>
                         </div>
 
                         <div className="form-group social-input">
-                            <i className="fab fa-youtube fa-2x"></i>
-                            <input type="text" placeholder="YouTube URL" name="youtube" value={youtube} onChange={e => onChange(e)}/>
+                            <i className="fab fa-youtube fa-2x"/>
+                            <input type="text" placeholder="YouTube URL" name="youtube" value={youtube}
+                                   onChange={e => onChange(e)}/>
                         </div>
 
                         <div className="form-group social-input">
-                            <i className="fab fa-linkedin fa-2x"></i>
-                            <input type="text" placeholder="Linkedin URL" name="linkedin" value={linkedin} onChange={e => onChange(e)}/>
+                            <i className="fab fa-linkedin fa-2x"/>
+                            <input type="text" placeholder="Linkedin URL" name="linkedin" value={linkedin}
+                                   onChange={e => onChange(e)}/>
                         </div>
 
                         <div className="form-group social-input">
-                            <i className="fab fa-instagram fa-2x"></i>
-                            <input type="text" placeholder="Instagram URL" name="instagram" value={instagram} onChange={e => onChange(e)}/>
+                            <i className="fab fa-instagram fa-2x"/>
+                            <input type="text" placeholder="Instagram URL" name="instagram" value={instagram}
+                                   onChange={e => onChange(e)}/>
                         </div>
                     </Fragment>
                 )}
@@ -162,4 +167,4 @@ CreateProfile.propTypes = {
 };
 
 // the withRouter is required to pass in the history prop
-export default connect(null, { createProfile })(withRouter(CreateProfile));
+export default connect(null, {createProfile})(withRouter(CreateProfile));
