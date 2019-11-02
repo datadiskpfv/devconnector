9f5421e789696fad13f4c94b2cef05dade25bbbd
datadiskpfv <paul.valle@datadisk.co.uk>
2019-11-02 14:12:40 +0000

    working on the profile

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,13 +1,12 @@
 import axios from 'axios';
-import { setAlert } from './alert';
 
 import {
     GET_PROFILE,
     PROFILE_ERROR
 } from "./types";
 
-// Get currect users profile
-export const getCurrentProfile = () = async dispatch => {
+// Get current users profile
+export const getCurrentProfile = () => async dispatch => {
     try {
         const res = await axios.get('/api/profile/me');
 
@@ -16,6 +15,9 @@ export const getCurrentProfile = () = async dispatch => {
            payload: res.data
         });
     } catch (err) {
-
+        dispatch ({
+            type: PROFILE_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        });
     }
 };
