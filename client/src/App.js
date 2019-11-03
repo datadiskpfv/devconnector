232bef6a41533e5020461bd6272dbce77038b488
datadiskpfv <paul.valle@datadisk.co.uk>
2019-11-03 09:53:03 +0000

    working on the profile, editing the profile

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import Alert from './components/layout/Alert';
 import Dashboard from "./components/dashboard/DashBoard";
 import PrivateRoute from "./components/routing/PrivateRoute";
 import CreateProfile from "./components/profile-forms/CreateProfile";
+import EditProfile from "./components/profile-forms/EditProfile";
 
 // Redux
 import {Provider} from 'react-redux';
@@ -41,6 +42,7 @@ const App = () => {
                             <Route exact path="/login" component={Login}/>
                             <PrivateRoute exact path="/dashboard" component={Dashboard}/>
                             <PrivateRoute exact path="/create-profile" component={CreateProfile}/>
+                            <PrivateRoute exact path="/edit-profile" component={EditProfile}/>
                         </Switch>
                     </section>
                 </Fragment>
