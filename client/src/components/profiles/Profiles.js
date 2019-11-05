0e1e41afadf3bef4b933cfb546645881170e72bb
datadiskpfv <paul.valle@datadisk.co.uk>
2019-11-05 20:15:10 +0000

    working on the profiles page

--- client/src/components/profiles/Profiles.js
+++ client/src/components/profiles/Profiles.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import { getProfiles } from "../../actions/profile";
+import ProfileItem from "./ProfileItem"
 
 const Profiles = ({ getProfiles, profile: { profiles, loading }}) => {
 
@@ -11,9 +12,20 @@ const Profiles = ({ getProfiles, profile: { profiles, loading }}) => {
     }, [getProfiles]);
 
     return (
-        <div>
-
-        </div>
+        <Fragment>
+            { loading ? <Spinner/> : <Fragment>
+                <h1 className="large text-primary">Developers</h1>
+                <p className="lead">
+                    <i className="fab fa-connectdevelop"/> Browser and connect with developers
+                </p>
+                <div className="profiles">
+                    {profiles.length > 0 ? (
+                        profiles.map(profile => (
+                            <ProfileItem key={profile._id} profile={profile} /> ))
+                    ) : <h4>No Profiles found</h4>}
+                </div>
+            </Fragment>}
+        </Fragment>
     );
 };
 
@@ -24,6 +36,6 @@ Profiles.propTypes = {
 
 const mapStateToProps = state => ({
     profile: state.profile
-})
+});
 
 export default connect(mapStateToProps, { getProfiles })(Profiles);
